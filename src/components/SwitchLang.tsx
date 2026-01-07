53304845188a17d6da7492713007b358813ff0d5
jachaussloef <w-jiacheng@hotmail.com>
2026-01-07 16:02:34 +0800

    添加对繁体中文（zh-HK）的语言支持

--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -25,6 +25,8 @@ const localeText = (locale: string): string => {
       return '🇪🇸 Español'
     case 'zh-CN':
       return '🇨🇳 简体中文'
+    case 'zh-HK':
+      return '🇭🇰 繁體中文'
     case 'hi':
       return '🇮🇳 हिन्दी'
     case 'id':
